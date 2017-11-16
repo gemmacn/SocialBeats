3ff3c581dffe3258f22d5f8b126eecbe0d093c9d
gemmacn <gmm.casanovas@gmail.com>
2017-11-16 11:38:12 +0100

    16/11

--- client/react/src/index.js
+++ client/react/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
+const {
+    Link,
+    Route,
+    BrowserRouter
+} = ReactRouterDOM
 ReactDOM.render(<App />, document.getElementById('root'));
 
